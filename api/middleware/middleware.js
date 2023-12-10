5d315fd799e820aeb874ad35d3228e6bf4f575eb
Calvin K <calvinknaub@gmail.com>
2023-12-09 19:03:36 -0800

    wip validateUser

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,39 +1,45 @@
-const User = require('../users/users-model')
+const User = require("../users/users-model");
 
 function logger(req, res, next) {
-  const timeStamp = new Date().toLocaleString()
-  const method = req.method
-  const url = req.originalUrl
-  console.log(`[${timeStamp}] ${method} to ${url}`)
-  next()
+  const timeStamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timeStamp}] ${method} to ${url}`);
+  next();
 }
 
 async function validateUserId(req, res, next) {
-  try { 
-    const user = await User.getById(req.params.id)
+  try {
+    const user = await User.getById(req.params.id);
     if (!user) {
       req.status(404).json({
-        message: 'no such user'
-      })
+        message: "user not found",
+      });
     } else {
-      req.user = user
-      next()
+      req.user = user;
+      next();
     }
   } catch (err) {
     res.status(500).json({
-      message: 'problem finding user'
-    })
+      message: "problem finding user",
+    });
   }
 }
 
 function validateUser(req, res, next) {
-  console.log("validateUser middleware");
-  next()
+  const { name } = req.body;
+  if (!name) {
+    req.status(400).json({
+      message: "missing required name field",
+    });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   console.log("validatePost middleware");
-  next()
+  next();
 }
 
 // do not forget to expose these functions to other modules
